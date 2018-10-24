9c0b16a987e7bdaf167679a2f14af5ca57c612ca
MrTheGood <maarten@degoedeweb.nl>
2018-10-24 02:04:21 +0200

    Added autoStop support

--- src/eu/insertcode/wordgames/games/LongWordGame.java
+++ src/eu/insertcode/wordgames/games/LongWordGame.java
@@ -5,21 +5,33 @@ import org.bukkit.Bukkit;
 import eu.insertcode.wordgames.Main;
 
 /**
- * The LongWordGame is any game that takes long enough for multiple game messages to be sent.
+ * The LongWordGame is any game that takes long enough for multiple game messages to be sent or for autoStop to kick in.
  * Pretty much every game except [TimedGame]
  */
 abstract class LongWordGame extends WordGame {
-	private int schedulerID;
+	private int repeatSendMessage, endWordGame;
 	
 	LongWordGame(Main instance, String wordToType, Reward reward) {
 		super(instance, wordToType, reward);
-		int time = plugin.getConfig().getInt("gameOptions.scheduler.timerInSeconds");
-		schedulerID = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, this::sendGameMessage, 20 * 10, time * 20);
+		
+		int repeatTime = plugin.getConfig().getInt("gameOptions.scheduler.timerInSeconds");
+		repeatSendMessage = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, this::sendGameMessage, 20 * 10, repeatTime * 20);
+		
+		if (plugin.getConfig().getBoolean("gameOptions.autoStop.enabled")) {
+			int endTime = plugin.getConfig().getInt("gameOptions.autoStop.timerInSeconds");
+			
+			endWordGame = Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+				for (String message : Main.getColouredMessages("games.autoStop.stop"))
+					Bukkit.broadcastMessage(message.replace("{word}", wordToType));
+				endGame();
+			}, endTime * 20);
+		}
 	}
 	
 	@Override
 	public void endGame() {
 		super.endGame();
-		Bukkit.getScheduler().cancelTask(schedulerID);
+		Bukkit.getScheduler().cancelTask(repeatSendMessage);
+		Bukkit.getScheduler().cancelTask(endWordGame);
 	}
 }
